99193edd49a2c0cb6fd4c6f795cfae33bfb291ec
joe-shajan <joeshajan1551@gmail.com>
2023-10-13 20:53:08 +0530

    remove build file

--- apps/api/dist/elastic-client.d.ts
+++
@@ -1,3 +0,0 @@
-import { Client } from "@elastic/elasticsearch";
-export declare const elasticClient: Client;
-//# sourceMappingURL=elastic-client.d.ts.map
--- apps/api/dist/seed-data.d.ts
+++
@@ -1,8 +0,0 @@
-export declare const seed_books: {
-    title: string;
-    author: string;
-    publicationYear: number;
-    isbn: string;
-    description: string;
-}[];
-//# sourceMappingURL=seed-data.d.ts.map
