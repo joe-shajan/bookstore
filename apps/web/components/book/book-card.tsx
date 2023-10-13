2d80920ba95265bab1951c64a019e641f39aae7b
joe-shajan <joeshajan1551@gmail.com>
2023-10-14 01:52:06 +0530

    fix: books card ui

--- apps/web/components/book/book-card.tsx
+++ apps/web/components/book/book-card.tsx
@@ -44,35 +44,37 @@ export function BookCard({
   });
 
   return (
-    <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/4">
-      <article className="bg-gray-50 overflow-hidden rounded-lg shadow-md hover:shadow-lg border-slate-100 border p-3 md:p-0 text-slate-700">
+    <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+      <article className="overflow-hidden h-[265px]  rounded-lg shadow-md hover:shadow-lg transition-all border-slate-100 border p-3 md:p-0 text-slate-700">
         <Link href={`/book/${book._id}`}>
-          <header className="leading-tight p-2 pb-1">
-            <div className="text-xl font-semibold flex items-center justify-center gap-3">
+          <header className="leading-tight p-2 pb-1 mt-4">
+            <div className="text-2xl font-semibold flex items-center justify-center gap-3 overflow-hidden">
               <span>
                 <BsBook />
               </span>
-              <span>{book.title}</span>
+              <h2 className="whitespace-nowrap">{book.title}</h2>
             </div>
-            <div className="  flex items-center gap-1 text-grey-darker text-sm mt-4 px-0 md:px-2 ">
-              <BsPen />
+            <div className="flex items-center gap-1 text-grey-darker text-base mt-6 justify-center px-0 md:px-2 ">
+              <BsPen size={15} />
               <p className="font-semibold">{book.author}</p>
             </div>
           </header>
         </Link>
-        <footer className="flex flex-col gap-1 leading-none px-2 md:px-4 pb-4 ">
+        <footer className="flex flex-col gap-1 leading-none px-2 md:px-4">
           <Link href={`/book/${book._id}`}>
-            <div className="flex items-center gap-1 text-sm">
-              <BsCalendar2Date />
+            <div className="flex items-center mt-1 justify-center gap-1 text-sm">
+              <BsCalendar2Date size={15} />
               <span className="font-semibold">{book.publicationYear}</span>
             </div>
-            <div className="text-sm items-center gap-1">
-              <div className="flex items-center gap-1 text-sm">
-                <MdOutlineDescription />
-                <span className="font-semibold">{book.description}</span>
+            <div className="text-sm items-center justify-center gap-1">
+              <div className="gap-1 mt-3 text-sm max-h-[40px] overflow-hidden text-ellipsis">
+                {/* <MdOutlineDescription size={18} /> */}
+                <p className="font-semibold text-center mx-4">
+                  {book.description}
+                </p>
               </div>
-              <div className="flex items-center gap-1 text-sm">
-                <CiBarcode />
+              <div className="flex items-center justify-center gap-1 mt-3 text-sm">
+                <CiBarcode size={18} />
                 <span className="font-semibold">{book.isbn}</span>
               </div>
             </div>
